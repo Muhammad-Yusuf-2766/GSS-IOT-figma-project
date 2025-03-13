2391a4acd743997b61b79764391411283ea2b0cc
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2025-03-13 19:10:50 +0900

    fixed ts build issue

--- src/test/orientation.tsx
+++ src/test/orientation.tsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
-// const socket = io("http://localhost:5000");
+// // const socket = io("http://localhost:5000");
 
-const OrientationDisplay = () => {
-	const [orientation, setOrientation] = useState({
-		horizontal: 0,
-		vertical: 0,
-	})
+// const OrientationDisplay = () => {
+// 	const [orientation, setOrientation] = useState({
+// 		horizontal: 0,
+// 		vertical: 0,
+// 	})
 
-	return (
-		<div className='flex flex-col items-center justify-center h-screen'>
-			<div
-				className='h-[400px] w-10 bg-blue-500 rounded-md'
-				style={{
-					transform: `rotateX(${orientation.vertical}deg) rotateY(${orientation.horizontal}deg) rotateZ(-30deg)`, // rotateZ(-30deg) -> Chapga 30° qiyaltiladi
-				}}
-			/>
-			<p>Horizontal: {orientation.horizontal}°</p>
-			<p>Vertical: {orientation.vertical}°</p>
-		</div>
-	)
-}
+// 	return (
+// 		<div className='flex flex-col items-center justify-center h-screen'>
+// 			<div
+// 				className='h-[400px] w-10 bg-blue-500 rounded-md'
+// 				style={{
+// 					transform: `rotateX(${orientation.vertical}deg) rotateY(${orientation.horizontal}deg) rotateZ(-30deg)`, // rotateZ(-30deg) -> Chapga 30° qiyaltiladi
+// 				}}
+// 			/>
+// 			<p>Horizontal: {orientation.horizontal}°</p>
+// 			<p>Vertical: {orientation.vertical}°</p>
+// 		</div>
+// 	)
+// }
 
-export default OrientationDisplay
+// export default OrientationDisplay
